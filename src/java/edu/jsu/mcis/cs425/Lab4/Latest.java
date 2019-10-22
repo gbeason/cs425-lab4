cf60dd4a9f4f6ba83b953be419113fe43c9b07ea
Trey Beason <trey.beason@yahoo.com>
2019-10-21 23:10:33 -0500

    Step 5 (Exception while accessing pool.  Problem with credentials I think.)

--- src/java/edu/jsu/mcis/cs425/Lab4/Latest.java
+++ src/java/edu/jsu/mcis/cs425/Lab4/Latest.java
@@ -3,6 +3,10 @@ package edu.jsu.mcis.cs425.Lab4;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,7 +26,14 @@ public class Latest extends HttpServlet{
         
         try (PrintWriter out = response.getWriter()) {
            
-            out.println(Rates.getRatesAsJson(Rates.getRates(path)));
+            //out.println(Rates.getRatesAsJson(Rates.getRates(path)));
+            
+            try {
+                out.println(Rates.getRatesAsJson( request.getParameter("code")) );
+            }
+            catch (NamingException | SQLException ex) {
+                Logger.getLogger(Latest.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
     
